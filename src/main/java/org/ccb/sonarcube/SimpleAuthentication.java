6e27c1ffa9382a0d9d7832fad5996ce12400dec2
Cevher <ccb@ccb.org>
2021-03-28 19:29:23 +0300

    fixing sonar issue

--- src/main/java/org/ccb/sonarcube/SimpleAuthentication.java
+++ src/main/java/org/ccb/sonarcube/SimpleAuthentication.java
@@ -3,9 +3,6 @@ package org.ccb.sonarcube;
 public class SimpleAuthentication {
 
 	public boolean authenticate(String username, String password) {
-		if ("user".equals(username) && "Test".equals(password)) {
-			return true;
-		}
-		return false;
+	 return ("user".equals(username) && "Test".equals(password));
  	}
 }
